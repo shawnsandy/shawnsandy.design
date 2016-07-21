6d86de004c419e39be12a20109fa4a0539e2ae6d
shawn sandy <shawnsandy@shawns-Mac-mini.local>
2016-07-20 23:46:58 -0400

    launch cover

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,8 +38,14 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">ShawnSandy.Design</div>
+               <h1> <img src="/img/site-address@2x.png" alt="www.shawnsandy.design"></h1>
+                <div class="title">
+                    <img src="/img/site-logo@2x.png" alt="logo">
+
+                </div>
+
             </div>
+           
         </div>
     </body>
 </html>
